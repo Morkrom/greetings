95fca9a66f934c8a12872e607934e0f5645ea1af
Michael <3208281+Morkrom@users.noreply.github.com>
2026-01-16 23:15:08 -0800

    save point experience section title

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,38 @@
 module Main exposing (main)
 
+--exposing (Html, button, div, text, a)
+--style, href)
+
 import Browser
-import Html exposing (Html, button, div, text, a)
-import Html.Attributes exposing (style, href)
-import Svg.Attributes exposing (scale)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import SwiftIcon exposing (swift)
-import ObjCIcon exposing (objectiveC)
-import Logo
 import Images
+import Logo
+import MorkromCss exposing (..)
+import ObjCIcon exposing (objectiveC)
+import Svg.Attributes exposing (scale)
+import SwiftIcon exposing (swift)
+
+
 
 -- Domain
 
+
 menuButton : Msg -> String -> Html Msg
 menuButton msg title =
-  button [onClick msg, 
-          style "padding" "10px",
-          style "border" "none", 
-          style "background" "white" ] 
-         [text title]
+    button
+        [ onClick msg
+        , style "padding" "10px"
+        , style "border" "none"
+        , style "background" "white"
+        ]
+        [ text title ]
+
 
 
 -- Application
+
 
 main : Program () Model Msg
 main =
@@ -31,231 +42,247 @@ main =
         , update = update
         }
 
+
+
 -- MODEL
 
-sections = ["greetings", "worksample"]
 
-type RoundedButton = Highlighted | Lowlighted
+sections =
+    [ "greetings", "worksample" ]
 
---"mailto:morkrom@icloud.com"
---file:///Users/apple/Software/morkrom/index.html
+
 roundedButton : String -> String -> RoundedButton -> Html Msg
-roundedButton title link buttonStyle = 
-  div [style "border-width" "1px", 
-       style "border-color" "rgb(2, 102, 223)", 
-       style "border-style" "solid", 
-       style "border-radius" "30px",
-       style "background-color" (buttonBackgroundColorString buttonStyle)]
-      [
-       a 
-         [href link,
-          style "font-family" "Arial", 
-          style "padding-left" "10px",
-          style "padding-right" "10px",
-          style "padding-top" "5px",
-          style "padding-bottom" "5px",
-          style "color" (buttonBackgroundColorTitleString buttonStyle),
-          style "display" "inline-block",
-          style "text-decoration" "none",
-          style "font-size" "12px"
-         ]
-          [text title]
-      ]
+roundedButton title link buttonStyle =
+    div
+        (roundButtonStyle
+            buttonStyle
+        )
+        [ a
+            [ href link
+            , style "font-family" "Arial"
+            , style "padding-left" "10px"
+            , style "padding-right" "10px"
+            , style "padding-top" "5px"
+            , style "padding-bottom" "5px"
+            , style "color" (buttonBackgroundColorTitleString buttonStyle)
+            , style "display" "inline-block"
+            , style "text-decoration" "none"
+            , style "font-size" "12px"
+            ]
+            [ text title ]
+        ]
 
-buttonBackgroundColorTitleString : RoundedButton -> String
-buttonBackgroundColorTitleString style =
-    case style of 
-      Highlighted -> 
-        "white"
-      Lowlighted ->
-         "rgb(2, 102, 223)" 
-
-buttonBackgroundColorString : RoundedButton -> String
-buttonBackgroundColorString style = 
-  case style of 
-    Highlighted -> 
-      "rgb(2, 102, 223)"
-    Lowlighted -> 
-      "transparent"
 
 type alias Model =
-    { appSections: List String, 
-      selectedSection: Int
+    { appSections : List String
+    , selectedSection : Int
     }
+
 
 init : Model
 init =
-    { appSections = sections,
-      selectedSection = 0
+    { appSections = sections
+    , selectedSection = 0
     }
+
+
 
 -- UPDATE
 
+
 type Msg
-    = Greetings | Work
+    = Greetings
+    | Work
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Greetings ->
             { model | selectedSection = 0 }
-        Work -> 
+
+        Work ->
             { model | selectedSection = 1 }
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div [style "position" "absolute", 
-       style "top" "0%",
-       style "width" "100%"] [ 
-    div [
-         style "display" "flex",
-         style "justify-content" "center",
-         style "gap" "10px",  
-         style "position" "fixed",
-         style "background-color" "rgba(255, 255, 255, 0.95)",
-         style "backdrop-filter" "blur(50px)",
-         style "top" "0",
-         style "left" "0",
-         style "right" "0"
-      ]
-      [
-        menuButton Greetings "Greetings",
-        menuButton Work "Work"
-      ],
-      div [style "background" "white", 
-           style "justify-content" "center", 
-           style "display" "flex", 
-           style "gap" "10px",
-           style "background-color" "white",
-           style "flex-direction" "column", 
-           style "margin-top" "20px"] 
-        ([introSection, languageGnostic])
---    div [style "background" "red"] [text (String.fromInt model.selectedSection)]
-  ]
+    div
+        [ style "position" "absolute"
+        , style "top" "0%"
+        , style "width" "100%"
+        ]
+        [ div
+            [ style "display" "flex"
+            , style "justify-content" "center"
+            , style "gap" "10px"
+            , style "position" "fixed"
+            , style "background-color" "rgba(255, 255, 255, 0.95)"
+            , style "backdrop-filter" "blur(50px)"
+            , style "top" "0"
+            , style "left" "0"
+            , style "right" "0"
+            ]
+            [ menuButton Greetings "Greetings"
+            , menuButton Work "Work"
+            ]
+        , div
+            [ style "background" "white"
+            , style "justify-content" "center"
+            , style "display" "flex"
+            , style "gap" "10px"
 
-introSectionDivStyle : List (Html.Attribute msg)
-introSectionDivStyle = 
-  [
-    style "justify-content" "center",
-    style "display" "flex",
-    style "gap" "0px",
-    style "flex-direction" "column", 
-    style "background" "rgb(243, 243, 246)"
-  ]
+            --, style "background-color" "white"
+            , style "flex-direction" "column"
+            , style "margin-top" "20px"
+            ]
+            [ introSection, languageGnostic, exp ]
+
+        --    div [style "background" "red"] [text (String.fromInt model.selectedSection)]
+        ]
+
 
 introSection : Html Msg
-introSection = div introSectionDivStyle
-  ((titleText "Michael Mork" "iOS Engineer") ++ [roundedButtons, titleSvgs])
+introSection =
+    div introSectionDivStyle
+        (titleText "Michael Mork" "iOS Engineer" ++ [ roundedButtons, titleSvgs ])
+
 
 roundedButtons : Html Msg
-roundedButtons = div [ 
-        style "justify-content" "center", 
-        style "display" "flex",
-        style "gap" "10px",
-        style "flex-direction" "row", 
-        style "background" "rgb(243, 243, 246)"]
-   [
-    --roundedButton "Résumé" "file:///Users/apple/Software/morkrom/resume-master-a.pdf" Highlighted, 
-    roundedButton "Contact" "mailto:morkrom@icloud.com" Lowlighted
-  ]
+roundedButtons =
+    div
+        [ style "justify-content" "center"
+        , style "display" "flex"
+        , style "gap" "10px"
+        , style "flex-direction" "row"
+        , style "background" "rgb(243, 243, 246)"
+        ]
+        [ --roundedButton "Résumé" "file:///Users/apple/Software/morkrom/resume-master-a.pdf" Highlighted,
+          roundedButton "Contact" "mailto:morkrom@icloud.com" Lowlighted
+        ]
+
 
 titleSvgs : Html Msg
-titleSvgs = div [
-    style "display" "block",
-    style "margin" "auto"
-  ] [
-      Images.phone 
-  ]
+titleSvgs =
+    div
+        [ style "display" "block"
+        , style "margin" "auto"
+        ]
+        [ Images.phone
+        ]
+
 
 titleTextSubtitle : String -> Html Msg
-titleTextSubtitle subtitle = 
-  
-  div [ 
-       style "justify-content" "center", 
-       style "display" "flex", style "gap" "1px",
-       style "flex-direction" "row", 
-       style "background" "rgb(243, 243, 246)"]
-  [
-    Html.h3 [style "margin" "10", 
-             style "font-family" "Arial",
-             style "line-height" "0px",
-             style "font-weight" "300"] [text subtitle]
-  ]
+titleTextSubtitle subtitle =
+    h3
+        subtitleTextStyle
+        [ text subtitle ]
+
 
 titleText : String -> String -> List (Html Msg)
-titleText title subtitle = 
-    [Html.h1 [
-              style "margin-top" "45px", 
-              style "font-family" "Arial", 
-              style "text-align" "center", 
-              style "line-height" "0px"] [text title], 
-     titleTextSubtitle subtitle
+titleText title subtitle =
+    [ h1
+        titleTextStyle
+        [ text title ]
+    , titleTextSubtitle subtitle
     ]
 
+
 languageGnostic : Html Msg
-languageGnostic = 
-     div introSectionDivStyle 
-     (
-      (titleText "Language" "Any paradigm any day.") ++
-      [roundedButtons] ++
-      [div [
-          style "justify-content" "center",
-          style "display" "flex", style "gap" "20px",
-          style "flex-direction" "row",
-          style "flex-wrap" "wrap",
-          style "background" "rgb(243, 243, 246)", style "margin-top" "20px"]
-        [ div [style "width" "200px"] [
-            swift [ ]],
-          div [style "width" "200px"] [ 
-            objectiveC [ ]],
-          div [style "width" "190px"] [
-            Logo.main
-        ]
-       ]
-     ])
+languageGnostic =
+    div introSectionDivStyle
+        (titleText "Language" "Any paradigm any day."
+            ++ [ roundedButtons ]
+            ++ [ div
+                    blockContents
+                    [ div [ style "width" "200px" ]
+                        [ swift []
+                        ]
+                    , div [ style "width" "200px" ]
+                        [ objectiveC []
+                        ]
+                    , div (smallBlockW 190.0)
+                        [ Logo.main
+                        ]
+                    ]
+               ]
+        )
+
 
 exp : Html Msg
-exp = 
-  div introSectionDivStyle
-  (  (titleText "Exp" "Experience is the teacher of all things. <br>- J. Caesar")
-  ++ [roundedButtons] ++ 
-  [ expBox "IV annus" 
-           (linkedExperience 
-             "Senior iOS Engineer (Core, Verticals, Specialist)" 
-             "https://thrivemarket.com")
-           "Main contributor and stability & performance lead. Satisfied KPIs: Reduce app load time by over 1 second - Eliminate app memory leaks - Own UI performance (scroll). Ads with Instacart - “Fresh” vertical main contributor - Implement Ai chat bot - Main contributor role to Webby winner “Sahara” product release, resulting in roughly 6% increase in sales for more than 1 million user base. Ownership of home screen. Implement Shopping List feature set. Contribute to every section: Autoship, quiz, PLP, PDP, review orders, cart. Deliver on a growing platform of over a million users. Modernize legacy sections. Increase UI & Unit coverage to ~40%. Horiziontal Screen and app load research and solutions using Instruments + New Relic."
-          ["SwiftUI", "UIKit", "Objective-C", "Swift", "Xcode Instruments", "XCTest", "NewRelic", "MVVM"]
-  ]
-  
-{-
- 52           style "font-family" "Arial",
- 53           style "padding-left" "10px",
- 54           style "padding-right" "10px",
- 55           style "padding-top" "5px",
- 56           style "padding-bottom" "5px",
- 57           style "color" (buttonBackgroundColorTitleString buttonStyle),
- 58           style "display" "inline-block",
- 59           style "text-decoration" "none",
- 60           style "font-size" "12px"
--}
+exp =
+    div introSectionDivStyle
+        (titleText "Exp" "\"Experience is the teacher of all things.\" - Julius Caesar"
+            ++ [ roundedButtons ]
+            ++ [ div blockContents
+                    [ expBox "6 yr"
+                        (linkedExperience
+                            "Senior iOS Engineer (Core, Verticals, Specialist)"
+                            "https://thrivemarket.com"
+                        )
+                        "Main contributor and stability & performance lead. Satisfied KPIs: Reduce app load time by over 1 second - Eliminate app memory leaks - Own UI performance (scroll). Ads with Instacart - “Fresh” vertical main contributor - Implement Ai chat bot - Main contributor role to Webby winner “Sahara” product release, resulting in roughly 6% increase in sales for more than 1 million user base. Ownership of home screen. Implement Shopping List feature set. Contribute to every section: Autoship, quiz, PLP, PDP, review orders, cart. Deliver weekly for a growing platform of over a million users. Modernize legacy sections. Increase UI & Unit coverage to ~40%. Horiziontal Screen and app load research and solutions using Instruments + New Relic."
+                        [ "SwiftUI", "UIKit", "Objective-C", "Swift", "Xcode Instruments", "XCTest", "NewRelic", "MVVM" ]
+                        []
+                    ]
+               ]
+        )
 
-{-
-typealias ReferenceQuote = Html Msg
+
+type alias ReferenceQuote =
+    Html Msg
+
+
 referenceQuote : String -> String -> ReferenceQuote
-referenceQuote quote referee = 
--}
+referenceQuote quote referee =
+    p technologyStyle [ text (quote ++ "\n-" ++ referee) ]
+
+
 
 -- title, linkout
-typealias LinkedExperienceTitle : String -> String -> Html Msg
-linkedExperience title : LinkedExperienceTitle
+
+
+type alias LinkedExperienceTitle =
+    Html Msg
+
+
+linkedExperience : String -> String -> LinkedExperienceTitle
 linkedExperience title link =
-  a [ href link
-    , myExperienceLinkCSS
-    ] 
-    [ text title 
-    ] 
+    a
+        ([ href link
+         , attribute "target" "_blank"
+         ]
+            ++ myExperienceLinkCSS
+        )
+        [ text title
+        ]
 
-expBox : String -> LinkedExperinceTitle -> String -> List String -> ReferenceQuote -> Html Msg
 
+technologyHtml : String -> Html msg
+technologyHtml technology =
+    div technologyStyle [ text technology ]
+
+
+technologiesHtml : List String -> Html msg
+technologiesHtml techs =
+    div blockContentsR
+        (List.map technologyHtml techs)
+
+
+expBox : String -> LinkedExperienceTitle -> String -> List String -> List ReferenceQuote -> Html Msg
+expBox duration titleAndLink bodyText technologies references =
+    div
+        expBoxStyle
+        [ div [ style "margin" "10px" ]
+            ([ titleAndLink
+             , br [] []
+             , text duration
+             , p technologyStyleW [ text bodyText ]
+             ]
+                ++ [ technologiesHtml technologies ]
+                ++ references
+            )
+        ]
--- src/MorkromCss.elm
+++ src/MorkromCss.elm
@@ -1,15 +1,158 @@
-module MorkromCss exposing (main)
+module MorkromCss exposing (..)
 
-import Css exposing (..)
-import Html
-import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (css, href, src)
-import Html.Styled.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
-myExperienceLinkCSS : Html.Styled.Attributes
+
+type TextColor
+    = LightBlue
+    | AppleBlue
+    | LightestGray
+
+
+type RoundedButton
+    = Highlighted
+    | Lowlighted
+
+
+buttonColor : TextColor -> String
+buttonColor color =
+    case color of
+        LightBlue ->
+            "rgb(120, 197, 239)"
+
+        LightestGray ->
+            "rgb(229, 229, 234)"
+
+        AppleBlue ->
+            "rgb(2, 102, 223)"
+
+
+roundButtonStyle : RoundedButton -> List (Attribute msg)
+roundButtonStyle buttonStyle =
+    [ style "border-width" "1px"
+    , style "border-color" (buttonColor AppleBlue)
+    , style "border-style" "solid"
+    , style "border-radius" "30px"
+    , style "background-color" (buttonBackgroundColorString buttonStyle)
+    ]
+
+
+buttonBackgroundColorTitleString : RoundedButton -> String
+buttonBackgroundColorTitleString style =
+    case style of
+        Highlighted ->
+            "white"
+
+        Lowlighted ->
+            "rgb(2, 102, 223)"
+
+
+buttonBackgroundColorString : RoundedButton -> String
+buttonBackgroundColorString style =
+    case style of
+        Highlighted ->
+            "rgb(2, 102, 223)"
+
+        Lowlighted ->
+            "transparent"
+
+
+technologyStyleW : List (Attribute msg)
+technologyStyleW =
+    [ style "font-family" "Arial"
+    , style "color" (buttonColor LightestGray)
+    , style "font-size" "12px"
+    , style "text-align" "left"
+    ]
+
+
+technologyStyleBordered : List (Attribute msg)
+technologyStyleBordered =
+    [] ++ technologyStyle
+
+
+technologyStyle : List (Attribute msg)
+technologyStyle =
+    [ style "font-family" "Arial"
+    , style "color" (buttonColor LightBlue) --"rgb(2, 102, 223)"
+    , style "font-size" "12px"
+    ]
+
+
+myExperienceLinkCSS : List (Attribute msg)
 myExperienceLinkCSS =
-  css [ fontFamily "Arial"
-      , textDecoration none
-      , fontSize (px 14)
-      , display inlineBlock,
-      , color rgb (2, 102, 223)]
+    [ style "font-family" "Arial"
+    , style "text-decoration" "none"
+    , style "font-size" "14px"
+    , style "display" "inlineBlock"
+    , style "color" (buttonColor LightBlue) --"rgb(2, 102, 223)"
+    ]
+
+
+introSectionDivStyle : List (Attribute msg)
+introSectionDivStyle =
+    [ style "justify-content" "center"
+    , style "display" "flex"
+    , style "gap" "0px"
+    , style "flex-direction" "column"
+    , style "background" "rgb(243, 243, 246)"
+    ]
+
+
+smallBlock : List (Attribute msg)
+smallBlock =
+    [ style "width" "200.0px"
+    ]
+
+
+smallBlockW : Float -> List (Attribute msg)
+smallBlockW w =
+    [ style "width" (String.fromFloat w ++ "px")
+    ]
+
+
+blockContents : List (Attribute msg)
+blockContents =
+    blockContentsR ++ [ style "background" "rgb(243, 243, 246)" ]
+
+
+blockContentsR : List (Attribute msg)
+blockContentsR =
+    [ style "justify-content" "center"
+    , style "display" "flex"
+    , style "gap" "20px"
+    , style "flex-direction" "row"
+    , style "flex-wrap" "wrap"
+    , style "margin-top" "20px"
+    ]
+
+
+expBoxStyle : List (Attribute msg)
+expBoxStyle =
+    [ style "background" "#1D1D1F"
+    , style "border-radius" "20px"
+    ]
+        ++ smallBlockW 260
+
+
+titleTextStyle : List (Attribute msg)
+titleTextStyle =
+    [ style "margin-top" "45px"
+    , style "font-family" "Arial"
+    , style "text-align" "center"
+    , style "line-height" "0px"
+    , style "word-break" "break-word"
+    ]
+
+
+subtitleTextStyle : List (Attribute msg)
+subtitleTextStyle =
+    [ style "margin" "10"
+    , style "font-family" "Arial"
+    , style "font-weight" "300"
+    , style "word-break" "break-word"
+    , style "display" "inline-block"
+    , style "text-align" "center"
+    ]
