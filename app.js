5df8f39b65d1ad61831df7901915b16e0966bb73
Michael <3208281+Morkrom@users.noreply.github.com>
2026-02-01 12:58:46 -0800

    dnb svg more

--- app.js
+++ app.js
@@ -6570,21 +6570,253 @@ var $author$project$AppleseGallerySlide$slideList = F2(
 	function (toSelf, component) {
 		return A2($author$project$AppleseGallerySlide$slideText, toSelf, component);
 	});
-var $elm$svg$Svg$Attributes$offset = _VirtualDom_attribute('offset');
 var $elm$svg$Svg$trustedNode = _VirtualDom_nodeNS('http://www.w3.org/2000/svg');
+var $elm$svg$Svg$animate = $elm$svg$Svg$trustedNode('animate');
+var $elm$svg$Svg$Attributes$attributeName = _VirtualDom_attribute('attributeName');
+var $elm$svg$Svg$Attributes$begin = _VirtualDom_attribute('begin');
 var $elm$svg$Svg$defs = $elm$svg$Svg$trustedNode('defs');
-var $elm$svg$Svg$Attributes$fill = _VirtualDom_attribute('fill');
+var $elm$svg$Svg$Attributes$dur = _VirtualDom_attribute('dur');
 var $elm$svg$Svg$Attributes$height = _VirtualDom_attribute('height');
-var $elm$svg$Svg$Attributes$id = _VirtualDom_attribute('id');
 var $elm$svg$Svg$linearGradient = $elm$svg$Svg$trustedNode('linearGradient');
-var $author$project$GallerySlideImages$phoneVB = '0 0 720 720';
+var $elm$virtual_dom$VirtualDom$nodeNS = F2(
+	function (namespace, tag) {
+		return A2(
+			_VirtualDom_nodeNS,
+			namespace,
+			_VirtualDom_noScript(tag));
+	});
+var $elm$svg$Svg$node = $elm$virtual_dom$VirtualDom$nodeNS('http://www.w3.org/2000/svg');
 var $elm$svg$Svg$Attributes$preserveAspectRatio = _VirtualDom_attribute('preserveAspectRatio');
 var $elm$svg$Svg$rect = $elm$svg$Svg$trustedNode('rect');
+var $elm$svg$Svg$Attributes$repeatCount = _VirtualDom_attribute('repeatCount');
+var $elm$svg$Svg$Attributes$restart = _VirtualDom_attribute('restart');
 var $elm$svg$Svg$Attributes$rx = _VirtualDom_attribute('rx');
 var $elm$svg$Svg$Attributes$ry = _VirtualDom_attribute('ry');
+var $elm$svg$Svg$stop = $elm$svg$Svg$trustedNode('stop');
 var $elm$svg$Svg$svg = $elm$svg$Svg$trustedNode('svg');
-var $elm$svg$Svg$Attributes$viewBox = _VirtualDom_attribute('viewBox');
+var $elm$svg$Svg$Attributes$values = function (value) {
+	return A2(
+		_VirtualDom_attribute,
+		'values',
+		_VirtualDom_noJavaScriptUri(value));
+};
 var $elm$svg$Svg$Attributes$width = _VirtualDom_attribute('width');
+var $author$project$DNBIcon$brainSvgBigZoomie = A2(
+	$elm$svg$Svg$svg,
+	_List_fromArray(
+		[
+			A2($elm$virtual_dom$VirtualDom$attribute, 'x', '0px'),
+			A2($elm$virtual_dom$VirtualDom$attribute, 'y', '0px'),
+			A2($elm$virtual_dom$VirtualDom$attribute, 'viewBox', '0 0 463 463'),
+			$elm$svg$Svg$Attributes$width('100%'),
+			$elm$svg$Svg$Attributes$height('100%'),
+			$elm$svg$Svg$Attributes$preserveAspectRatio('none'),
+			$elm$svg$Svg$Attributes$rx('20'),
+			$elm$svg$Svg$Attributes$ry('20')
+		]),
+	_List_fromArray(
+		[
+			A2(
+			$elm$svg$Svg$defs,
+			_List_Nil,
+			_List_fromArray(
+				[
+					A2(
+					$elm$svg$Svg$linearGradient,
+					_List_fromArray(
+						[
+							A2($elm$virtual_dom$VirtualDom$attribute, 'id', 'grad'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'x1', '0'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'x2', '1'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'y1', '1'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'y2', '0')
+						]),
+					_List_fromArray(
+						[
+							A2(
+							$elm$svg$Svg$stop,
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'offset', '25%'),
+									A2($elm$virtual_dom$VirtualDom$attribute, 'stop-color', '#40e8cc')
+								]),
+							_List_fromArray(
+								[
+									A2(
+									$elm$svg$Svg$animate,
+									_List_fromArray(
+										[
+											$elm$svg$Svg$Attributes$attributeName('stop-color'),
+											$elm$svg$Svg$Attributes$values('#40e8cc;#e61e45;#40e8cc'),
+											$elm$svg$Svg$Attributes$dur('1.8s'),
+											$elm$svg$Svg$Attributes$repeatCount('0'),
+											$elm$svg$Svg$Attributes$begin('indefinite'),
+											$elm$svg$Svg$Attributes$restart('whenNotActive')
+										]),
+									_List_Nil)
+								])),
+							A2(
+							$elm$svg$Svg$stop,
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'offset', '75%'),
+									A2($elm$virtual_dom$VirtualDom$attribute, 'stop-color', '#e61e45')
+								]),
+							_List_fromArray(
+								[
+									A2(
+									$elm$svg$Svg$animate,
+									_List_fromArray(
+										[
+											$elm$svg$Svg$Attributes$attributeName('stop-color'),
+											$elm$svg$Svg$Attributes$values('#e61e45;#40e8cc;#e61e45;'),
+											$elm$svg$Svg$Attributes$dur('1.8s'),
+											$elm$svg$Svg$Attributes$repeatCount('0'),
+											$elm$svg$Svg$Attributes$begin('indefinite')
+										]),
+									_List_Nil)
+								]))
+						])),
+					A2(
+					$elm$svg$Svg$linearGradient,
+					_List_fromArray(
+						[
+							A2($elm$virtual_dom$VirtualDom$attribute, 'id', 'grad1'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'x1', '0'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'x2', '1'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'y1', '1'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'y2', '0')
+						]),
+					_List_fromArray(
+						[
+							A2(
+							$elm$svg$Svg$stop,
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'offset', '30%'),
+									A2($elm$virtual_dom$VirtualDom$attribute, 'stop-color', '#e61e45')
+								]),
+							_List_fromArray(
+								[
+									A2(
+									$elm$svg$Svg$animate,
+									_List_fromArray(
+										[
+											$elm$svg$Svg$Attributes$attributeName('stop-color'),
+											$elm$svg$Svg$Attributes$values('#e61e45;#40e8cc;#e61e45'),
+											$elm$svg$Svg$Attributes$dur('1.7s'),
+											$elm$svg$Svg$Attributes$repeatCount('0'),
+											$elm$svg$Svg$Attributes$begin('indefinite')
+										]),
+									_List_Nil)
+								])),
+							A2(
+							$elm$svg$Svg$stop,
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'offset', '60%'),
+									A2($elm$virtual_dom$VirtualDom$attribute, 'stop-color', '#40e8cc')
+								]),
+							_List_fromArray(
+								[
+									A2(
+									$elm$svg$Svg$animate,
+									_List_fromArray(
+										[
+											$elm$svg$Svg$Attributes$attributeName('stop-color'),
+											$elm$svg$Svg$Attributes$values('#40e8cc;#e61e45;#40e8cc'),
+											$elm$svg$Svg$Attributes$dur('1.7s'),
+											$elm$svg$Svg$Attributes$repeatCount('0'),
+											$elm$svg$Svg$Attributes$begin('indefinite')
+										]),
+									_List_Nil)
+								]))
+						]))
+				])),
+			A2(
+			$elm$svg$Svg$rect,
+			_List_fromArray(
+				[
+					A2($elm$virtual_dom$VirtualDom$attribute, 'x', '0px'),
+					A2($elm$virtual_dom$VirtualDom$attribute, 'y', '0px'),
+					A2($elm$virtual_dom$VirtualDom$attribute, 'width', '463px'),
+					A2($elm$virtual_dom$VirtualDom$attribute, 'height', '463px'),
+					A2($elm$virtual_dom$VirtualDom$attribute, 'fill', 'url(\'#grad\')'),
+					$elm$svg$Svg$Attributes$rx('20'),
+					$elm$svg$Svg$Attributes$ry('20')
+				]),
+			_List_Nil),
+			A2(
+			$elm$svg$Svg$svg,
+			_List_fromArray(
+				[
+					A2($elm$virtual_dom$VirtualDom$attribute, 'x', '0px'),
+					A2($elm$virtual_dom$VirtualDom$attribute, 'y', '0px'),
+					A2($elm$virtual_dom$VirtualDom$attribute, 'viewBox', '10 100 200 200')
+				]),
+			_List_fromArray(
+				[
+					A3(
+					$elm$svg$Svg$node,
+					'g',
+					_List_fromArray(
+						[
+							A2($elm$virtual_dom$VirtualDom$attribute, 'fill', 'url(\'#grad1\')'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'x', '0px'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'y', '0px'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'width', '463px'),
+							A2($elm$virtual_dom$VirtualDom$attribute, 'height', '463px')
+						]),
+					_List_fromArray(
+						[
+							A3(
+							$elm$svg$Svg$node,
+							'path',
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'd', 'M151.245,222.446C148.054,237.039,135.036,248,119.5,248c-4.142,0-7.5,3.357-7.5,7.5s3.358,7.5,7.5,7.5\n\t\t\t\tc23.774,0,43.522-17.557,46.966-40.386c14.556-1.574,27.993-8.06,38.395-18.677c2.899-2.959,2.85-7.708-0.109-10.606\n\t\t\t\tc-2.958-2.897-7.707-2.851-10.606,0.108C184.947,202.829,172.643,208,159.5,208c-26.743,0-48.5-21.757-48.5-48.5\n\t\t\t\tc0-4.143-3.358-7.5-7.5-7.5s-7.5,3.357-7.5,7.5C96,191.715,120.119,218.384,151.245,222.446z')
+								]),
+							_List_Nil),
+							A3(
+							$elm$svg$Svg$node,
+							'path',
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'd', 'M183,287.5c0-4.143-3.358-7.5-7.5-7.5c-35.014,0-63.5,28.486-63.5,63.5c0,0.362,0.013,0.725,0.019,1.088\n\t\t\t\tC109.23,344.212,106.39,344,103.5,344c-4.142,0-7.5,3.357-7.5,7.5s3.358,7.5,7.5,7.5c26.743,0,48.5,21.757,48.5,48.5\n\t\t\t\tc0,4.143,3.358,7.5,7.5,7.5s7.5-3.357,7.5-7.5c0-26.611-16.462-49.437-39.731-58.867c-0.178-1.699-0.269-3.418-0.269-5.133\n\t\t\t\tc0-26.743,21.757-48.5,48.5-48.5C179.642,295,183,291.643,183,287.5z')
+								]),
+							_List_Nil),
+							A3(
+							$elm$svg$Svg$node,
+							'path',
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'd', 'M439,223.5c0-17.075-6.82-33.256-18.875-45.156c1.909-6.108,2.875-12.426,2.875-18.844\n\t\t\t\tc0-30.874-22.152-56.659-51.394-62.329C373.841,91.6,375,85.628,375,79.5c0-19.557-11.883-36.387-28.806-43.661\n\t\t\t\tC317.999,13.383,287.162,0,263.5,0c-13.153,0-24.817,6.468-32,16.384C224.317,6.468,212.653,0,199.5,0\n\t\t\t\tc-23.662,0-54.499,13.383-82.694,35.839C99.883,43.113,88,59.943,88,79.5c0,6.128,1.159,12.1,3.394,17.671\n\t\t\t\tC62.152,102.841,40,128.626,40,159.5c0,6.418,0.965,12.735,2.875,18.844C30.82,190.244,24,206.425,24,223.5\n\t\t\t\tc0,13.348,4.149,25.741,11.213,35.975C27.872,270.087,24,282.466,24,295.5c0,23.088,12.587,44.242,32.516,55.396\n\t\t\t\tC56.173,353.748,56,356.626,56,359.5c0,31.144,20.315,58.679,49.79,68.063C118.611,449.505,141.965,463,167.5,463\n\t\t\t\tc27.995,0,52.269-16.181,64-39.674c11.731,23.493,36.005,39.674,64,39.674c25.535,0,48.889-13.495,61.71-35.437\n\t\t\t\tc29.475-9.385,49.79-36.92,49.79-68.063c0-2.874-0.173-5.752-0.516-8.604C426.413,339.742,439,318.588,439,295.5\n\t\t\t\tc0-13.034-3.872-25.413-11.213-36.025C434.851,249.241,439,236.848,439,223.5z M167.5,448c-21.029,0-40.191-11.594-50.009-30.256\n\t\t\t\tc-0.973-1.849-2.671-3.208-4.688-3.751C88.19,407.369,71,384.961,71,359.5c0-3.81,0.384-7.626,1.141-11.344\n\t\t\t\tc0.702-3.447-1.087-6.92-4.302-8.35C50.32,332.018,39,314.626,39,295.5c0-8.699,2.256-17.014,6.561-24.379\n\t\t\t\tC56.757,280.992,71.436,287,87.5,287c4.142,0,7.5-3.357,7.5-7.5s-3.358-7.5-7.5-7.5C60.757,272,39,250.243,39,223.5\n\t\t\t\tc0-14.396,6.352-27.964,17.428-37.221c2.5-2.09,3.365-5.555,2.14-8.574C56.2,171.869,55,165.744,55,159.5\n\t\t\t\tc0-26.743,21.757-48.5,48.5-48.5s48.5,21.757,48.5,48.5c0,4.143,3.358,7.5,7.5,7.5s7.5-3.357,7.5-7.5\n\t\t\t\tc0-33.642-26.302-61.243-59.421-63.355C104.577,91.127,103,85.421,103,79.5c0-13.369,8.116-24.875,19.678-29.859\n\t\t\t\tc0.447-0.133,0.885-0.307,1.308-0.527C127.568,47.752,131.447,47,135.5,47c12.557,0,23.767,7.021,29.256,18.325\n\t\t\t\tc1.81,3.727,6.298,5.281,10.023,3.47c3.726-1.809,5.28-6.296,3.47-10.022c-6.266-12.903-18.125-22.177-31.782-25.462\n\t\t\t\tC165.609,21.631,184.454,15,199.5,15c13.509,0,24.5,10.99,24.5,24.5v97.051c-6.739-5.346-15.25-8.551-24.5-8.551\n\t\t\t\tc-4.142,0-7.5,3.357-7.5,7.5s3.358,7.5,7.5,7.5c13.509,0,24.5,10.99,24.5,24.5v180.279c-9.325-12.031-22.471-21.111-37.935-25.266\n\t\t\t\tc-3.999-1.071-8.114,1.297-9.189,5.297c-1.075,4.001,1.297,8.115,5.297,9.189C206.8,343.616,224,366.027,224,391.5\n\t\t\t\tC224,422.654,198.654,448,167.5,448z M395.161,339.807c-3.215,1.43-5.004,4.902-4.302,8.35c0.757,3.718,1.141,7.534,1.141,11.344\n\t\t\t\tc0,25.461-17.19,47.869-41.803,54.493c-2.017,0.543-3.716,1.902-4.688,3.751C335.691,436.406,316.529,448,295.5,448\n\t\t\t\tc-31.154,0-56.5-25.346-56.5-56.5c0-2.109-0.098-4.2-0.281-6.271c0.178-0.641,0.281-1.314,0.281-2.012V135.5\n\t\t\t\tc0-13.51,10.991-24.5,24.5-24.5c4.142,0,7.5-3.357,7.5-7.5s-3.358-7.5-7.5-7.5c-9.25,0-17.761,3.205-24.5,8.551V39.5\n\t\t\t\tc0-13.51,10.991-24.5,24.5-24.5c15.046,0,33.891,6.631,53.033,18.311c-13.657,3.284-25.516,12.559-31.782,25.462\n\t\t\t\tc-1.81,3.727-0.256,8.214,3.47,10.022c3.726,1.81,8.213,0.257,10.023-3.47C303.733,54.021,314.943,47,327.5,47\n\t\t\t\tc4.053,0,7.933,0.752,11.514,2.114c0.422,0.22,0.86,0.393,1.305,0.526C351.883,54.624,360,66.13,360,79.5\n\t\t\t\tc0,5.921-1.577,11.627-4.579,16.645C322.302,98.257,296,125.858,296,159.5c0,4.143,3.358,7.5,7.5,7.5s7.5-3.357,7.5-7.5\n\t\t\t\tc0-26.743,21.757-48.5,48.5-48.5s48.5,21.757,48.5,48.5c0,6.244-1.2,12.369-3.567,18.205c-1.225,3.02-0.36,6.484,2.14,8.574\n\t\t\t\tC417.648,195.536,424,209.104,424,223.5c0,26.743-21.757,48.5-48.5,48.5c-4.142,0-7.5,3.357-7.5,7.5s3.358,7.5,7.5,7.5\n\t\t\t\tc16.064,0,30.743-6.008,41.939-15.879c4.306,7.365,6.561,15.68,6.561,24.379C424,314.626,412.68,332.018,395.161,339.807z')
+								]),
+							_List_Nil),
+							A3(
+							$elm$svg$Svg$node,
+							'path',
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'd', 'M359.5,240c-15.536,0-28.554-10.961-31.745-25.554C358.881,210.384,383,183.715,383,151.5c0-4.143-3.358-7.5-7.5-7.5\n\t\t\t\ts-7.5,3.357-7.5,7.5c0,26.743-21.757,48.5-48.5,48.5c-13.143,0-25.447-5.171-34.646-14.561c-2.898-2.958-7.647-3.007-10.606-0.108\n\t\t\t\ts-3.008,7.647-0.109,10.606c10.402,10.617,23.839,17.103,38.395,18.677C315.978,237.443,335.726,255,359.5,255\n\t\t\t\tc4.142,0,7.5-3.357,7.5-7.5S363.642,240,359.5,240z')
+								]),
+							_List_Nil),
+							A3(
+							$elm$svg$Svg$node,
+							'path',
+							_List_fromArray(
+								[
+									A2($elm$virtual_dom$VirtualDom$attribute, 'd', 'M335.5,328c-2.89,0-5.73,0.212-8.519,0.588c0.006-0.363,0.019-0.726,0.019-1.088c0-35.014-28.486-63.5-63.5-63.5\n\t\t\t\tc-4.142,0-7.5,3.357-7.5,7.5s3.358,7.5,7.5,7.5c26.743,0,48.5,21.757,48.5,48.5c0,1.714-0.091,3.434-0.269,5.133\n\t\t\t\tC288.462,342.063,272,364.889,272,391.5c0,4.143,3.358,7.5,7.5,7.5s7.5-3.357,7.5-7.5c0-26.743,21.757-48.5,48.5-48.5\n\t\t\t\tc4.142,0,7.5-3.357,7.5-7.5S339.642,328,335.5,328z')
+								]),
+							_List_Nil)
+						]))
+				]))
+		]));
+var $elm$svg$Svg$Attributes$offset = _VirtualDom_attribute('offset');
+var $elm$svg$Svg$Attributes$fill = _VirtualDom_attribute('fill');
+var $elm$svg$Svg$Attributes$id = _VirtualDom_attribute('id');
+var $author$project$GallerySlideImages$phoneVB = '0 0 720 720';
+var $elm$svg$Svg$Attributes$viewBox = _VirtualDom_attribute('viewBox');
 var $elm$svg$Svg$Attributes$x = _VirtualDom_attribute('x');
 var $elm$svg$Svg$Attributes$x1 = _VirtualDom_attribute('x1');
 var $elm$svg$Svg$Attributes$x2 = _VirtualDom_attribute('x2');
@@ -6635,28 +6867,7 @@ var $author$project$GallerySlideImages$presize = function (colorString) {
 				_List_Nil)
 			]));
 };
-var $elm$svg$Svg$stop = $elm$svg$Svg$trustedNode('stop');
 var $elm$svg$Svg$Attributes$stopColor = _VirtualDom_attribute('stop-color');
-var $author$project$GallerySlideImages$dnb = $author$project$GallerySlideImages$presize(
-	_List_fromArray(
-		[
-			A2(
-			$elm$svg$Svg$stop,
-			_List_fromArray(
-				[
-					$elm$svg$Svg$Attributes$offset('0%'),
-					$elm$svg$Svg$Attributes$stopColor('#40e8cc')
-				]),
-			_List_Nil),
-			A2(
-			$elm$svg$Svg$stop,
-			_List_fromArray(
-				[
-					$elm$svg$Svg$Attributes$offset('75%'),
-					$elm$svg$Svg$Attributes$stopColor('#e61e45')
-				]),
-			_List_Nil)
-		]));
 var $author$project$GallerySlideImages$izonit = $author$project$GallerySlideImages$presize(
 	_List_fromArray(
 		[
@@ -6708,7 +6919,7 @@ var $author$project$GallerySlideImages$res = $author$project$GallerySlideImages$
 var $author$project$AppleseGallerySlide$slideLogo = function (data) {
 	switch (data.$) {
 		case 'Dnb':
-			return $author$project$GallerySlideImages$dnb;
+			return $author$project$DNBIcon$brainSvgBigZoomie;
 		case 'IzOn':
 			return $author$project$GallerySlideImages$izonit;
 		default:
@@ -6745,7 +6956,9 @@ var $author$project$AppleseGallerySlide$slideContent = F2(
 							_List_fromArray(
 								[
 									A2($elm$html$Html$Attributes$style, 'width', componentData.svgSize.w),
-									A2($elm$html$Html$Attributes$style, 'height', componentData.svgSize.h)
+									A2($elm$html$Html$Attributes$style, 'height', componentData.svgSize.h),
+									A2($elm$html$Html$Attributes$style, 'min-width', componentData.svgSize.w),
+									A2($elm$html$Html$Attributes$style, 'min-height', componentData.svgSize.h)
 								]),
 							_List_fromArray(
 								[
@@ -6889,18 +7102,7 @@ var $author$project$Images$phoneApps = function (size) {
 			},
 			$author$project$Images$phoneRows));
 };
-var $elm$svg$Svg$animate = $elm$svg$Svg$trustedNode('animate');
-var $elm$svg$Svg$Attributes$attributeName = _VirtualDom_attribute('attributeName');
-var $elm$svg$Svg$Attributes$begin = _VirtualDom_attribute('begin');
-var $elm$svg$Svg$Attributes$dur = _VirtualDom_attribute('dur');
 var $author$project$Images$phoneVB = '0 0 150 312';
-var $elm$svg$Svg$Attributes$repeatCount = _VirtualDom_attribute('repeatCount');
-var $elm$svg$Svg$Attributes$values = function (value) {
-	return A2(
-		_VirtualDom_attribute,
-		'values',
-		_VirtualDom_noJavaScriptUri(value));
-};
 var $author$project$Images$toPhoneScreenElement = function (element) {
 	return A2(
 		$elm$svg$Svg$svg,
@@ -6920,7 +7122,9 @@ var $author$project$Images$toPhoneScreenElement = function (element) {
 						$elm$core$String$fromFloat(element.y)),
 						$elm$svg$Svg$Attributes$width('20'),
 						$elm$svg$Svg$Attributes$height('20'),
-						$elm$svg$Svg$Attributes$fill('blue')
+						$elm$svg$Svg$Attributes$fill('blue'),
+						$elm$svg$Svg$Attributes$rx('3px'),
+						$elm$svg$Svg$Attributes$ry('3px')
 					]),
 				_List_Nil),
 				A2(
@@ -7075,14 +7279,6 @@ var $author$project$Logo$main = A2(
 				]),
 			_List_Nil)
 		]));
-var $elm$virtual_dom$VirtualDom$nodeNS = F2(
-	function (namespace, tag) {
-		return A2(
-			_VirtualDom_nodeNS,
-			namespace,
-			_VirtualDom_noScript(tag));
-	});
-var $elm$svg$Svg$node = $elm$virtual_dom$VirtualDom$nodeNS('http://www.w3.org/2000/svg');
 var $author$project$ObjCIcon$objectiveC = function (attrs) {
 	return A3(
 		$elm$svg$Svg$node,
